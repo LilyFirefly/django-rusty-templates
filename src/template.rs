9f16ea6bc0370339b34f19cadb33109d7be33447
Lily Acorn <code@lilyf.org>
2025-12-06 13:40:38 +0000

    Add select_template helper function

--- src/template.rs
+++ src/template.rs
@@ -277,6 +277,27 @@ pub mod django_rusty_templates {
         )))
     }
 
+    pub fn select_template(
+        engine: Arc<Engine>,
+        py: Python<'_>,
+        template_name_list: Vec<String>,
+    ) -> PyResult<Template> {
+        if template_name_list.is_empty() {
+            return Err(TemplateDoesNotExist::new_err("No template names provided"));
+        }
+        let mut not_found = Vec::new();
+        for template_name in template_name_list {
+            match get_template(engine.clone(), py, Cow::Owned(template_name)) {
+                Ok(template) => return Ok(template),
+                Err(e) if e.is_instance_of::<TemplateDoesNotExist>(py) => {
+                    not_found.push(e.value(py).to_string());
+                }
+                Err(e) => return Err(e),
+            }
+        }
+        Err(TemplateDoesNotExist::new_err(not_found.join(", ")))
+    }
+
     #[derive(Debug)]
     #[pyclass(name = "Engine")]
     pub struct PyEngine {
@@ -384,20 +405,7 @@ pub mod django_rusty_templates {
             py: Python<'_>,
             template_name_list: Vec<String>,
         ) -> PyResult<Template> {
-            if template_name_list.is_empty() {
-                return Err(TemplateDoesNotExist::new_err("No template names provided"));
-            }
-            let mut not_found = Vec::new();
-            for template_name in template_name_list {
-                match get_template(self.engine.clone(), py, Cow::Owned(template_name)) {
-                    Ok(template) => return Ok(template),
-                    Err(e) if e.is_instance_of::<TemplateDoesNotExist>(py) => {
-                        not_found.push(e.value(py).to_string());
-                    }
-                    Err(e) => return Err(e),
-                }
-            }
-            Err(TemplateDoesNotExist::new_err(not_found.join(", ")))
+            select_template(self.engine.clone(), py, template_name_list)
         }
 
         #[allow(clippy::wrong_self_convention)] // We're implementing a Django interface
