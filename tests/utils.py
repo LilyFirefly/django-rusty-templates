54aa6a217d2aed798db507219e168f9dee74ceb8
Pravin Kamble <iampbkamble@gmail.com>
2025-12-23 15:00:23 +0530

    Add helper function for generating rusty error messages

--- tests/utils.py
+++ tests/utils.py
@@ -19,3 +19,30 @@ class BrokenDunderHtml(str):
 class BrokenDunderBool:
     def __bool__(self):
         1 / 0
+
+
+def get_rusty_error(template, header, target, sub_label="here"):
+    """
+    Automated Miette-style error generator.
+    """
+    offset = template.find(target)
+    if offset == -1:
+        offset = 0
+
+    width = len(target)
+
+    side_dashes_count = (width - 1) // 2
+    side_dashes = "─" * side_dashes_count
+    pointer = f"{side_dashes}┬{side_dashes}"
+
+    label_padding = " " * (offset + side_dashes_count)
+    padding = " " * offset
+
+    return f"""\
+  × {header}
+   ╭────
+ 1 │ {template}
+   · {padding}{pointer}
+   · {label_padding}╰── {sub_label}
+   ╰────
+"""
