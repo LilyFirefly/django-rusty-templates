5fd92276707aed071e6ca322b26ff591c37aaecb
Lily Acorn <code@lilyf.org>
2026-01-08 11:13:08 +0000

    Implement Render for Block

--- src/parse.rs
+++ src/parse.rs
@@ -626,7 +626,7 @@ pub struct Extends {
 #[derive(Clone, Debug, PartialEq)]
 pub struct Block {
     name: String,
-    nodes: Vec<TokenTree>,
+    pub nodes: Vec<TokenTree>,
 }
 
 #[derive(Clone, Debug)]
