1960f01aed2efb674a0794d584763d4fe2d35c1c
Lily Acorn <code@lilyf.org>
2026-01-08 07:02:04 +0000

    Implement {% block %} and {% endblock %} parser

--- src/render/tags.rs
+++ src/render/tags.rs
@@ -21,9 +21,9 @@ use super::types::{
 use super::{Evaluate, Render, RenderResult, Resolve, ResolveFailures, ResolveResult};
 use crate::error::{AnnotatePyErr, PyRenderError, RenderError};
 use crate::parse::{
-    CsrfToken, Cycle, CycleValue, Extends, FirstOf, For, IfCondition, Include, IncludeTemplateName,
-    Lorem, NamedCycle, SilentNamedCycle, SimpleBlockTag, SimpleCycle, SimpleTag, Tag, TagElement,
-    Url,
+    Block, CsrfToken, Cycle, CycleValue, Extends, FirstOf, For, IfCondition, Include,
+    IncludeTemplateName, Lorem, NamedCycle, SilentNamedCycle, SimpleBlockTag, SimpleCycle,
+    SimpleTag, Tag, TagElement, Url,
 };
 use crate::path::construct_relative_path;
 use crate::template::django_rusty_templates::{NoReverseMatch, Template, TemplateDoesNotExist};
@@ -678,6 +678,7 @@ impl Render for Tag {
                     falsey.render(py, template, context)?
                 }
             }
+            Self::Block(block_tag) => block_tag.render(py, template, context)?,
             Self::Extends(extends_tag) => extends_tag.render(py, template, context)?,
             Self::For(for_tag) => for_tag.render(py, template, context)?,
             Self::Include(include_tag) => include_tag.render(py, template, context)?,
@@ -1041,6 +1042,17 @@ impl Render for Include {
 }
 
 impl Render for Extends {
+    fn render<'t>(
+        &self,
+        _py: Python,
+        _template: TemplateString<'t>,
+        _context: &mut Context,
+    ) -> RenderResult<'t> {
+        todo!()
+    }
+}
+
+impl Render for Block {
     fn render<'t>(
         &self,
         _py: Python,
--- tests/tags/test_extends.py
+++ tests/tags/test_extends.py
@@ -91,3 +91,76 @@ def test_extends_extra_argument(assert_parse_error):
     assert_parse_error(
         template=template, django_message=django_message, rusty_message=rusty_message
     )
+
+
+def test_block_too_many_arguments(assert_parse_error):
+    template = "{% extends 'base.txt' %}{% block body with extra arguments %}Some content{% endblock %}"
+    django_message = snapshot("'block' tag takes only one argument")
+    rusty_message = snapshot("""\
+  × 'block' tag takes only one argument
+   ╭────
+ 1 │ {% extends 'base.txt' %}{% block body with extra arguments %}Some content{% endblock %}
+   ·                                       ──────────┬─────────
+   ·                                                 ╰── unexpected argument(s)
+   ╰────
+""")
+    assert_parse_error(
+        template=template, django_message=django_message, rusty_message=rusty_message
+    )
+
+
+def test_endblock_too_many_arguments(assert_parse_error):
+    template = "{% extends 'base.txt' %}{% block body %}Some content{% endblock body with extra arguments %}"
+    django_message = snapshot(
+        "Invalid block tag on line 1: 'endblock', expected 'endblock' or 'endblock body'. Did you forget to register or load this tag?"
+    )
+    rusty_message = snapshot("""\
+  × 'endblock' tag takes only one argument
+   ╭────
+ 1 │ {% extends 'base.txt' %}{% block body %}Some content{% endblock body with extra arguments %}
+   ·                                                                      ──────────┬─────────
+   ·                                                                                ╰── unexpected argument(s)
+   ╰────
+""")
+    assert_parse_error(
+        template=template, django_message=django_message, rusty_message=rusty_message
+    )
+
+
+def test_endblock_wrong_name(assert_parse_error):
+    template = (
+        "{% extends 'base.txt' %}{% block body %}Some content{% endblock other %}"
+    )
+    django_message = snapshot(
+        "Invalid block tag on line 1: 'endblock', expected 'endblock' or 'endblock body'. Did you forget to register or load this tag?"
+    )
+    rusty_message = snapshot("""\
+  × Unexpected tag 'endblock other', expected 'endblock' or 'endblock body'
+   ╭────
+ 1 │ {% extends 'base.txt' %}{% block body %}Some content{% endblock other %}
+   ·                         ────────┬───────            ──────────┬─────────
+   ·                                 │                             ╰── unexpected tag
+   ·                                 ╰── start tag
+   ╰────
+""")
+    assert_parse_error(
+        template=template, django_message=django_message, rusty_message=rusty_message
+    )
+
+
+def test_extends_unexpected_endblock(assert_parse_error):
+    template = "{% extends 'base.txt' %}{% endblock foo %}"
+    django_message = snapshot(
+        "Invalid block tag on line 1: 'endblock'. Did you forget to register or load this tag?"
+    )
+    rusty_message = snapshot("""\
+  × Unexpected tag 'endblock foo'
+   ╭────
+ 1 │ {% extends 'base.txt' %}{% endblock foo %}
+   ·                         ─────────┬────────
+   ·                                  ╰── unexpected tag
+   ╰────
+""")
+    assert_parse_error(
+        template=template, django_message=django_message, rusty_message=rusty_message
+    )
