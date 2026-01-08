17f57e6966133c0451a6db9f0499f9ee47ebbcff
Pravin Kamble <iampbkamble@gmail.com>
2026-01-08 20:54:05 +0530

    Break the lexer into multiple methods

--- dtl-lexer/src/tag/now.rs
+++ dtl-lexer/src/tag/now.rs
@@ -1,5 +1,6 @@
-use crate::common::NextChar;
+use crate::common::{LexerError, check_variable_attrs};
 use crate::tag::TagParts;
+use crate::tag::common::{TagElementLexer, TagElementToken};
 use crate::types::{At, TemplateString};
 use miette::{Diagnostic, SourceSpan};
 use thiserror::Error;
@@ -10,95 +11,72 @@ pub struct Now {
     pub asvar: Option<At>,
 }
 
-#[derive(Debug, PartialEq, Clone)]
-pub enum NowTokenType {
-    Format,
-    As,
-    Variable,
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub struct NowToken {
-    pub at: At,
-    pub token_type: NowTokenType,
-}
-
 pub struct NowLexer<'t> {
-    rest: &'t str,
-    byte: usize,
-    seen_format: bool,
-    seen_as: bool,
-    seen_variable: bool,
+    template: TemplateString<'t>,
+    lexer: TagElementLexer<'t>,
+    parts: TagParts,
 }
 
 impl<'t> NowLexer<'t> {
     pub fn new(template: TemplateString<'t>, parts: TagParts) -> Self {
-        let content = template.content(parts.at);
-        let skip = content.next_non_whitespace();
         Self {
-            rest: &content[skip..],
-            byte: parts.at.0 + skip,
-            seen_format: false,
-            seen_as: false,
-            seen_variable: false,
+            template,
+            lexer: TagElementLexer::new(template, parts.clone()),
+            parts,
         }
     }
-}
 
-impl<'t> Iterator for NowLexer<'t> {
-    type Item = Result<NowToken, NowError>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.rest.is_empty() {
-            return None;
+    fn next_element(&mut self) -> Result<Option<TagElementToken>, NowError> {
+        match self.lexer.next() {
+            None => Ok(None),
+            Some(Ok(token)) => Ok(Some(token)),
+            Some(Err(err)) => Err(err.into()),
         }
+    }
 
-        let first_char = self.rest.chars().next()?;
+    pub fn lex_format(&mut self) -> Result<At, NowError> {
+        let Some(token) = self.next_element()? else {
+            return Err(NowError::Syntax {
+                at: self.parts.at.into(),
+            });
+        };
+        Ok(token.at)
+    }
 
-        let len = if first_char == '"' || first_char == '\'' {
-            match self.rest[1..].find(first_char) {
-                Some(end_idx) => end_idx + 2, // here +1 for skip and +1 for closing quote
-                None => self.rest.len(),      // Unclosed quote
+    pub fn lex_variable(&mut self) -> Result<Option<At>, NowError> {
+        let Some(token) = self.next_element()? else {
+            return Ok(None);
+        };
+
+        match self.template.content(token.at) {
+            "as" => {
+                let Some(var) = self.next_element()? else {
+                    return Err(NowError::Syntax {
+                        at: token.at.into(),
+                    });
+                };
+
+                check_variable_attrs(self.template.content(var.at), var.at.0)?;
+
+                Ok(Some(var.at))
             }
-        } else {
-            self.rest.next_whitespace()
-        };
+            _ => Err(NowError::Syntax {
+                at: token.at.into(),
+            }),
+        }
+    }
 
-        let at = (self.byte, len);
-        let content = &self.rest[..len];
-
-        let token_type = if !self.seen_format {
-            self.seen_format = true;
-            Ok(NowTokenType::Format)
-        } else if content == "as" && !self.seen_as {
-            self.seen_as = true;
-            Ok(NowTokenType::As)
-        } else if self.seen_as && !self.seen_variable {
-            self.seen_variable = true;
-            Ok(NowTokenType::Variable)
-        } else {
-            Err(NowError::Syntax { at: at.into() })
-        };
-
-        let token_type = match token_type {
-            Ok(t) => t,
-            Err(e) => {
-                self.rest = "";
-                return Some(Err(e));
-            }
-        };
-
-        let rest = &self.rest[len..];
-        let next_skip = rest.next_non_whitespace();
-        self.rest = &rest[next_skip..];
-        self.byte = self.byte + len + next_skip;
-
-        Some(Ok(NowToken { at, token_type }))
+    pub fn extra_token(&mut self) -> Result<Option<TagElementToken>, NowError> {
+        self.next_element()
     }
 }
 
 #[derive(Debug, Diagnostic, Error, PartialEq, Eq)]
 pub enum NowError {
+    #[error(transparent)]
+    #[diagnostic(transparent)]
+    LexerError(#[from] LexerError),
+
     #[error("'now' statement takes one argument")]
     Syntax {
         #[label("here")]
