faf03d035cebf52c5cf0165aa86ab2ea3234dc6d
Pravin Kamble <iampbkamble@gmail.com>
2026-01-27 18:41:30 +0530

    Added tests to increase coverage

--- dtl-lexer/src/tag/comment.rs
+++ dtl-lexer/src/tag/comment.rs
@@ -1,7 +1,7 @@
+use crate::common::LexerError;
+use crate::tag::TagParts;
 use crate::tag::common::{TagElementLexer, TagElementToken};
 use crate::types::TemplateString;
-use crate::tag::TagParts;
-use crate::common::LexerError;
 
 pub struct CommentLexer<'t> {
     lexer: TagElementLexer<'t>,
@@ -20,5 +20,29 @@ impl<'t> Iterator for CommentLexer<'t> {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.lexer.next()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::types::IntoTemplateString;
+
+    #[test]
+    fn test_empty_comment() {
+        let template = "{% comment %}{% endcomment %}";
+        let parts = TagParts { at: (12, 0) };
+        let mut lexer = CommentLexer::new(template.into_template_string(), parts);
+        assert!(lexer.next().is_none());
+    }
+
+    #[test]
+    fn test_comment_with_args() {
+        let template = "{% comment \"note\" word %}";
+        // "quote" (6) + space (1) + word (4) = 11
+        let parts = TagParts { at: (11, 11) };
+        let lexer = CommentLexer::new(template.into_template_string(), parts);
+        let tokens: Vec<_> = lexer.collect::<Result<Vec<_>, _>>().unwrap();
+        assert_eq!(tokens.len(), 2);
     }
 }
--- src/render/comment.rs
+++ src/render/comment.rs
@@ -3,3 +3,13 @@ use std::borrow::Cow;
 pub fn render_comment() -> Cow<'static, str> {
     Cow::Borrowed("")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_render_comment() {
+        assert_eq!(render_comment(), "");
+    }
+}
