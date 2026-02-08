7ab956c56affeefe83f60a65ed13b0d4b4e10b8e
Lily <code@lilyf.org>
2026-02-08 00:11:39 +0000

    Add error handling for invalid template name in {% extends %}

--- src/parse.rs
+++ src/parse.rs
@@ -384,7 +384,11 @@ fn parse_extends_template_token(
         TagElementTokenType::Variable => IncludeTemplateName::Variable(
             parser.parse_variable_or_filter(content, content_at, start)?,
         ),
-        TagElementTokenType::Numeric => std::todo!(),
+        TagElementTokenType::Numeric => {
+            return Err(ParseError::InvalidTemplateName {
+                at: content_at.into(),
+            });
+        }
         TagElementTokenType::TranslatedText => std::todo!(),
     })
 }
@@ -1121,8 +1125,8 @@ pub enum ParseError {
         at: SourceSpan,
     },
 
-    #[error("Invalid variable name")]
-    InvalidVariableName {
+    #[error("Template name must be a string or a variable")]
+    InvalidTemplateName {
         #[label("here")]
         at: SourceSpan,
     },
