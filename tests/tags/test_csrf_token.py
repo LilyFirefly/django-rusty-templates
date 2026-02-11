87165291cab3a8d7bb22e2ce3732869c262c14eb
Josh Thomas <josh@joshthomas.dev>
2026-02-11 11:36:58 -0600

    Use assert_render and render_output fixtures in csrf_token tests
    Simplify most tests by using the project's standard pytest fixtures
    which handle parametrization over both engines automatically.

--- tests/tags/test_csrf_token.py
+++ tests/tags/test_csrf_token.py
@@ -38,114 +38,76 @@ def test_csrf_token_context_processor(engine_class):
     )
 
 
-def test_csrf_token_basic():
-    template = "{% csrf_token %}"
-
-    django_template = engines["django"].from_string(template)
-    rust_template = engines["rusty"].from_string(template)
-
-    context = {"csrf_token": "test123"}
-    expected = '<input type="hidden" name="csrfmiddlewaretoken" value="test123">'
-
-    assert django_template.render(context) == expected
-    assert rust_template.render(context) == expected
+def test_csrf_token_basic(assert_render):
+    assert_render(
+        template="{% csrf_token %}",
+        context={"csrf_token": "test123"},
+        expected='<input type="hidden" name="csrfmiddlewaretoken" value="test123">',
+    )
 
 
-def test_csrf_token_not_provided():
-    template = "{% csrf_token %}"
-
-    django_template = engines["django"].from_string(template)
-    rust_template = engines["rusty"].from_string(template)
-
-    context = {"csrf_token": "NOTPROVIDED"}
-
-    assert django_template.render(context) == ""
-    assert rust_template.render(context) == ""
+def test_csrf_token_not_provided(assert_render):
+    assert_render(
+        template="{% csrf_token %}",
+        context={"csrf_token": "NOTPROVIDED"},
+        expected="",
+    )
 
 
-def test_csrf_token_missing():
-    template = "{% csrf_token %}"
-
-    django_template = engines["django"].from_string(template)
-    rust_template = engines["rusty"].from_string(template)
-
-    assert django_template.render({}) == ""
-    assert rust_template.render({}) == ""
+def test_csrf_token_missing(assert_render):
+    assert_render(
+        template="{% csrf_token %}",
+        context={},
+        expected="",
+    )
 
 
-def test_csrf_token_escaping():
-    template = "{% csrf_token %}"
-
-    django_template = engines["django"].from_string(template)
-    rust_template = engines["rusty"].from_string(template)
-
-    context = {"csrf_token": 'test"with<quotes>&amp;'}
-
-    django_result = django_template.render(context)
-    rust_result = rust_template.render(context)
-
-    assert django_result == rust_result
-    assert 'value="test&quot;with&lt;quotes&gt;&amp;amp;"' in rust_result
+def test_csrf_token_escaping(render_output):
+    result = render_output(
+        template="{% csrf_token %}",
+        context={"csrf_token": 'test"with<quotes>&amp;'},
+    )
+    assert 'value="test&quot;with&lt;quotes&gt;&amp;amp;"' in result
 
 
-def test_csrf_token_empty_string():
-    template = "{% csrf_token %}"
-
-    django_template = engines["django"].from_string(template)
-    rust_template = engines["rusty"].from_string(template)
-
-    context = {"csrf_token": ""}
-
-    assert django_template.render(context) == ""
-    assert rust_template.render(context) == ""
+def test_csrf_token_empty_string(assert_render):
+    assert_render(
+        template="{% csrf_token %}",
+        context={"csrf_token": ""},
+        expected="",
+    )
 
 
-def test_csrf_token_none_value():
-    template = "{% csrf_token %}"
-
-    django_template = engines["django"].from_string(template)
-    rust_template = engines["rusty"].from_string(template)
-
-    context = {"csrf_token": None}
-
-    assert django_template.render(context) == ""
-    assert rust_template.render(context) == ""
+def test_csrf_token_none_value(assert_render):
+    assert_render(
+        template="{% csrf_token %}",
+        context={"csrf_token": None},
+        expected="",
+    )
 
 
-def test_csrf_token_numeric_value():
-    template = "{% csrf_token %}"
-    django_template = engines["django"].from_string(template)
-    rust_template = engines["rusty"].from_string(template)
-
-    context = {"csrf_token": 12345}
-    expected = '<input type="hidden" name="csrfmiddlewaretoken" value="12345">'
-
-    assert django_template.render(context) == expected
-    assert rust_template.render(context) == expected
+def test_csrf_token_numeric_value(assert_render):
+    assert_render(
+        template="{% csrf_token %}",
+        context={"csrf_token": 12345},
+        expected='<input type="hidden" name="csrfmiddlewaretoken" value="12345">',
+    )
 
 
-def test_csrf_token_zero_value():
-    template = "{% csrf_token %}"
-
-    django_template = engines["django"].from_string(template)
-    rust_template = engines["rusty"].from_string(template)
-
-    context = {"csrf_token": 0}
-
-    assert django_template.render(context) == ""
-    assert rust_template.render(context) == ""
+def test_csrf_token_zero_value(assert_render):
+    assert_render(
+        template="{% csrf_token %}",
+        context={"csrf_token": 0},
+        expected="",
+    )
 
 
-def test_csrf_token_false_value():
-    template = "{% csrf_token %}"
-
-    django_template = engines["django"].from_string(template)
-    rust_template = engines["rusty"].from_string(template)
-
-    context = {"csrf_token": False}
-
-    assert django_template.render(context) == ""
-    assert rust_template.render(context) == ""
+def test_csrf_token_false_value(assert_render):
+    assert_render(
+        template="{% csrf_token %}",
+        context={"csrf_token": False},
+        expected="",
+    )
 
 
 @override_settings(DEBUG=True)
