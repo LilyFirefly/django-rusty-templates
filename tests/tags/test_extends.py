33dbf0be503f7836690f2d37ee3ee97e7a284896
Lily <code@lilyf.org>
2026-02-11 23:34:11 +0000

    Allow passing a translatable string into an {% extends %} tag
    Django doesn't forbid this, so neither should we.

--- tests/tags/test_extends.py
+++ tests/tags/test_extends.py
@@ -2,6 +2,7 @@ import pytest
 from django.template import TemplateSyntaxError
 from django.template.base import VariableDoesNotExist
 from django.template.exceptions import TemplateDoesNotExist
+from django.utils.translation import gettext_lazy
 from inline_snapshot import snapshot
 
 
@@ -34,6 +35,17 @@ def test_extends_variable(assert_render):
     assert_render(
         template=template,
         context={"template_name": "base.txt"},
+        expected="# Header\nSome content\n",
+    )
+
+
+def test_extends_variable_lazy_translation(assert_render):
+    template = (
+        "{% extends template_name %}{% block body %}Some content{% endblock body %}"
+    )
+    assert_render(
+        template=template,
+        context={"template_name": gettext_lazy("base.txt")},
         expected="# Header\nSome content\n",
     )
 
