89211020e8e68000a583d5778be1c489e09b1123
Pravin Kamble <iampbkamble@gmail.com>
2026-02-18 20:48:17 +0530

    .gitignore updated

--- llvm-cov-env.sh
+++
@@ -1,9 +0,0 @@
-export LLVM_PROFILE_FILE='/home/pravin/OpenSource/django-rusty-templates/target/django-rusty-templates-%p-%4m.profraw'
-export __CARGO_LLVM_COV_RUSTC_WRAPPER=1
-export __CARGO_LLVM_COV_RUSTC_WRAPPER_RUSTFLAGS='-Cinstrument-coverage--cfg=coverage'
-export __CARGO_LLVM_COV_RUSTC_WRAPPER_CRATE_NAMES=dtl_lexer,dtl_lexer_tests,dtl_lexer,dtl_benchmark,dtl_benchmark_tests,dtl_benchmark,slugify,django_rusty_templates,django_rusty_templates_tests,django_rusty_templates
-export RUSTC_WRAPPER=/home/pravin/.cargo/bin/cargo-llvm-cov
-export CARGO_LLVM_COV=1
-export CARGO_LLVM_COV_SHOW_ENV=1
-export CARGO_LLVM_COV_TARGET_DIR=/home/pravin/OpenSource/django-rusty-templates/target
-export CARGO_LLVM_COV_BUILD_DIR=/home/pravin/OpenSource/django-rusty-templates/target
